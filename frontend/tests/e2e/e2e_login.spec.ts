15e3f1ad97b0362ff7144b6538214770986ab424
Pulman <everybody.knows.1152@gmail.com>
2025-11-28 03:46:32 +0800

    fix: CI compose

--- frontend/tests/e2e/e2e_login.spec.ts
+++ frontend/tests/e2e/e2e_login.spec.ts
@@ -9,15 +9,25 @@ test('user can sign up, log in, see Logout, then log out', async ({ page }) => {
   await page.getByPlaceholder("Password").fill("pass123");
   await page.click("text=Sign up");
 
+  // if form
+  const cancelBtn = page.getByText('Cancel');
+  if (await cancelBtn.isVisible().catch(() => false)) {
+    await cancelBtn.click();
+  }
+
   // Log in
   await page.click("text=Log in");
   await page.getByPlaceholder("Email").fill("user@test.com");
   await page.getByPlaceholder("Password").fill("pass123");
-  await page.click("text=Log in");
+  await page.click("text=Login");
+
+  // if form
+  if (await cancelBtn.isVisible().catch(() => false)) {
+    await cancelBtn.click();
+  }
 
   await expect(page.getByText('Logout')).toBeVisible();
 
-  // Logout
   await page.click("text=Logout");
   await expect(page.getByText('Sign up')).toBeVisible();
 });
