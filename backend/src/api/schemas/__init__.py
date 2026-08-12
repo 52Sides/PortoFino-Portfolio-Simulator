91dd1b6be9e3f705ba7bdd2c0ee0cddd02634cfd
Dmitriy Pulyaev <Dmitriy.Pulyaev@ggmr.co.uk>
2026-08-12 17:06:29 +0800

    fix: deploy fix

---
+++ backend/src/api/schemas/__init__.py
@@ -0,0 +1,13 @@
+from api.schemas.simulation import (
+    ParsedCommandModel,
+    SimulateRequest,
+    SimulateResponse,
+    SimulateResponseWS,
+)
+
+__all__ = [
+    "ParsedCommandModel",
+    "SimulateRequest",
+    "SimulateResponse",
+    "SimulateResponseWS",
+]
